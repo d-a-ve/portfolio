c11e9c387d4abb8c1f50a3a56605cf14ad0214db
Dave <davearonmwan@gmail.com>
2026-06-06 18:29:18 +0100

    chore: add linkedin link to socials

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -4,15 +4,30 @@ import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
 export default function Socials() {
   return (
     <div className="socials">
-      <a href="https://github.com/d-a-ve/" target="_blank" rel="noreferrer noopener">
+      <a
+        href="https://github.com/d-a-ve/"
+        target="_blank"
+        rel="noreferrer noopener"
+        aria-label="GitHub"
+      >
         <FaGithub />
       </a>
-      <a href="https://twitter.com/kvng__dave" target="_blank" rel="noreferrer noopener">
+      <a
+        href="https://twitter.com/kvng__dave"
+        target="_blank"
+        rel="noreferrer noopener"
+        aria-label="Twitter"
+      >
         <FaTwitter />
       </a>
-      {/* <a href="#">
+      <a
+        href="https://www.linkedin.com/in/david-aronmwan-5101ab172/"
+        target="_blank"
+        rel="noreferrer noopener"
+        aria-label="LinkedIn"
+      >
         <FaLinkedin />
-      </a> */}
+      </a>
     </div>
   );
 }
